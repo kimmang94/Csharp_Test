51fd11c6b90b5b3e49a01c6a036bd47953b760ac
kimhyeok <kimmang940715@naver.com>
2024-05-04 22:01:10 +0900

    feat : 부분정리

---
+++ 010_StringClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_StringClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = " Hello World";
+            string t;
+
+            Console.WriteLine(s.Length);
+            Console.WriteLine(s[8]);
+            Console.WriteLine(s.Insert(8, "C# "));
+            Console.WriteLine(s.PadLeft(20, '.'));
+            Console.WriteLine(s.PadRight(20, '.'));
+            Console.WriteLine(s.Remove(6));
+        
+            Console.WriteLine(s.Replace('l', 'm'));
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine('/' + s.Trim() + '/');
+            Console.WriteLine('/' + s.TrimStart () + '/');
+            Console.WriteLine('/' + s.TrimEnd() + '/');
+
+            string[] a = s.Split(',');
+            foreach(var i in a)
+            {
+                Console.WriteLine('/' + i + '/');
+            }
+
+            char[] destination = new char[10];
+            s.CopyTo(8, destination, 0, 6);
+            Console.WriteLine(destination);
+            Console.ReadLine();
+        }
+    }
+}
